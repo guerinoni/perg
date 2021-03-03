e1e252c4b52d54fcae62ad5d60326b8e808d120d
Federico Guerinoni <guerinoni.federico@gmail.com>
2021-03-03 22:31:22 +0100

    Align behavior of simple grep to gnu grep tool.
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- README.md
+++ README.md
@@ -4,35 +4,9 @@
 
 Grep implementation in rust.
 
-## Feature
+## Features
 
 - [x] search in single file.
-- [x] search in folder.
-- [x] search in folder recursively.
-- [x] parallel search.
-- [ ] search in hidden folder.
-
-## Benchmarck 
-
-```zsh
-time grep -R federico /usr
-14,15s user 7,25s system 54% cpu 39,089 total
-
-time perg federico /usr
-21,04s user 4,96s system 750% cpu 3,467 total
-```
-
-```bash
-time grep -R federico /usr
-real    0m18,160s
-user    0m14,086s
-sys     0m3,385s
-
-time perg federico /usr
-real    0m6,337s
-user    0m27,545s
-sys     0m9,840s
-```
 
 ## Contributing
 
--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,6 @@ use std::{
 
 use rayon::iter::{IntoParallelIterator, ParallelBridge, ParallelIterator};
 
-use walkdir::WalkDir;
-
 pub struct Config<'a> {
     want_search: &'a str,
     filename: &'a str,
@@ -25,32 +23,44 @@ impl<'a> Config<'a> {
 pub fn grep(c: Config) {
     let path = path::Path::new(c.filename);
     if !path.exists() {
+        println!("No such file or directory");
         return;
     }
 
-    WalkDir::new(c.filename)
-        .into_iter()
-        .filter_entry(|e| !is_hidden(e))
-        .par_bridge()
-        .into_par_iter()
-        .filter_map(|rd| rd.ok())
-        .filter(|e| e.metadata().unwrap().is_file())
-        .for_each(|e| {
-            let file = fs::File::open(e.path()).unwrap();
-            let lines = io::BufReader::new(file).lines();
-            lines
-                .par_bridge()
-                .into_par_iter()
-                .filter_map(|i| i.ok())
-                .filter(|i| i.contains(c.want_search))
-                .for_each(|item| println!("{}: {}", e.path().display(), item));
-        })
+    if path.is_file() {
+        let file = fs::File::open(path).unwrap();
+        let lines = io::BufReader::new(file).lines();
+        lines
+            .par_bridge()
+            .into_par_iter()
+            .filter_map(|i| i.ok())
+            .filter(|i| i.contains(c.want_search))
+            .for_each(|item| println!("{}: {}", path.display(), item));
+    }
+
+    // WalkDir::new(c.filename)
+    //     .into_iter()
+    //     .filter_entry(|e| !is_hidden(e))
+    //     .par_bridge()
+    //     .into_par_iter()
+    //     .filter_map(|rd| rd.ok())
+    //     .filter(|e| e.metadata().unwrap().is_file())
+    //     .for_each(|e| {
+    //         let file = fs::File::open(e.path()).unwrap();
+    //         let lines = io::BufReader::new(file).lines();
+    //         lines
+    //             .par_bridge()
+    //             .into_par_iter()
+    //             .filter_map(|i| i.ok())
+    //             .filter(|i| i.contains(c.want_search))
+    //             .for_each(|item| println!("{}: {}", e.path().display(), item));
+    //     })
 }
 
-fn is_hidden(entry: &walkdir::DirEntry) -> bool {
-    entry
-        .file_name()
-        .to_str()
-        .map(|s| s.starts_with('.') && s.len() > 1)
-        .unwrap_or(false)
-}
+// fn is_hidden(entry: &walkdir::DirEntry) -> bool {
+//     entry
+//         .file_name()
+//         .to_str()
+//         .map(|s| s.starts_with('.') && s.len() > 1)
+//         .unwrap_or(false)
+// }
